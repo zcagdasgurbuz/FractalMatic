cea2d29abf5d4e26fd9940017ec010177b98ddea
Zeynel Cagdas Gurbuz <zcagdasgurbuz@gmail.com>
2021-07-04 16:01:56 -0700

    refactored project structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,13 +4,13 @@ module fractalmatic {
     requires com.jfoenix;
     opens fractalmatic to javafx.fxml;
     opens fractalmatic.circles.ui to javafx.fxml;
+    opens fractalmatic.common.ui to javafx.fxml;
     exports fractalmatic;
     exports fractalmatic.circles.animation;
     exports fractalmatic.circles.calculation;
     exports fractalmatic.circles.ui;
     exports fractalmatic.circles.config;
     exports fractalmatic.common.ui;
-    opens fractalmatic.common.ui to javafx.fxml;
     exports fractalmatic.common.animation;
     exports fractalmatic.common.util;
     exports fractalmatic.common.ui.util;
